f8ab104be8ac48ef604b4d9cbf25fe481fc105c2
Sarthak Kashyap <98842340+ROOKIEsarthak@users.noreply.github.com>
2024-02-10 01:03:41 +0530

    updated logical error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -140,7 +140,7 @@ const loginUser  = asyncHandler ( async(req,res) =>{
 
     // ------> check if username or email is present or not in order to login
 
-    if(!username && !email){
+    if(!(username || email)){
         throw new ApiError(400," username or email is required")
     }
 
@@ -235,7 +235,6 @@ const logoutUser = asyncHandler( async( req,res )=> {
             $set:{
                 refreshToken: undefined
             }
-
         },
         {
             new: true
