0500faeefe6f7ccd48e140a05a475e587d437ce6
Iambivert1 <skashyap@m0.ventures>
2024-06-06 12:44:09 +0530

    bug fixes and minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,98 +1,30 @@
 // ----------> THIS IS THE SERVER FILE . HERE ALL THE CONFIGURATION FOR THE SERVER IS DONE
 
-
-
-
-
-
 //require('dotenv').config({path:'./env'})
-import dotenv from 'dotenv'
-const port = process.env.PORT || 8000
+
+import dotenv from "dotenv";
+const port = process.env.PORT || 8000;
 import connectDb from "./db/index.js";
-import { app } from './app.js';
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
 connectDb()
-.then(()=>{
-        try 
-        {
-            app.listen(port, ()=>{
-            console.log(`Server running on port : ${port}`)});
-        } 
-        catch (error) 
-        {
-            console.error("ERROR: ",error);
-            throw err;
-        }
-        
+  .then(() => {
+    try {
+      app.listen(port, () => {
+        console.log(`Server running on port : ${port}`);
+      });
+    } catch (error) {
+      console.error("ERROR: ", error);
+      throw err;
     }
-)
-.catch((err)=>{
-    console.log("MongoDB connection failed !!! " , err);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!! ", err);
+  });
 
 /*
 import express from 'express';
